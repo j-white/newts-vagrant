34a42f445fac9dea3cb8438639d4eae95c1979fa
Jesse White <jwhite08@gmail.com>
2014-05-06 14:28:24 -0400

    Update the puppetlabs java module.

--- modules/java/manifests/params.pp
+++ modules/java/manifests/params.pp
@@ -76,7 +76,7 @@ class java::params {
             },
           }
         }
-        'wheezy', 'precise','quantal','raring','saucy': {
+        'wheezy', 'jessie', 'precise','quantal','raring','saucy', 'trusty': {
           $java =  {
             'jdk' => {
               'package'          => 'openjdk-7-jdk',
