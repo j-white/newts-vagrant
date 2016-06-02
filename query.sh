5a5d0ea0e16e9850fee9fe04bb51cc124fd8bcfd
Jesse White <jesse@opennms.org>
2016-06-02 12:06:24 -0400

    Update port to match the one in the Vagranfile.

--- query.sh
+++ query.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -D - -X GET 'http://0.0.0.0:8080/samples/localhost:chassis:temps?start=1998-07-09T12:05:00-0500&end=1998-07-09T13:15:00-0500'; echo
+curl -D - -X GET 'http://0.0.0.0:18080/samples/localhost:chassis:temps?start=1998-07-09T12:05:00-0500&end=1998-07-09T13:15:00-0500'; echo
